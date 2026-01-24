f46f69353330d3faff2c76fd646e54a786300e4f
Mateusz Poprawa <mateusz.poprawa@gmail.com>
2026-01-24 07:00:41 +0100

    converted to OOP

---
+++ Services/IUserService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Models;
+
+namespace UserManagement.Services
+{
+    internal interface IUserService
+    {
+        User Create(User user);
+        User? GetById(int id);
+
+        List<User> GetAll();
+        bool Remove(int id);
+        User Update(int id, string name);
+
+        List<User> GetUsersOlderThan(int age);
+
+        List<User> GetAllSortedByLastName();
+
+        List<User> GetActiveUsersSortedByAge();
+
+        List<string> GetUserEmails();
+
+        Dictionary<string, List<User>> GroupUsersByDepartment();
+
+        int CountAdmins();
+
+        User? GetUserByEmail(string email);
+
+        int GetMaxAge();
+
+        int GetAverageAge();
+
+        List<string GetDistinctDepartments();
+    }
+}
---
+++ Utils/ConsoleInputHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.Utils
+{
+    internal class ConsoleInputHelper
+    {
+        public static string ReadRequiredInput(string prompt)
+        {
+            string input;
+            do
+            {
+                Console.Write(prompt);
+                input = Console.ReadLine()?.Trim(); // remove leading/trailing spaces
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Error: This field is required. Please enter a value.");
+                }
+            } while (string.IsNullOrEmpty(input));
+
+            return input;
+        }
+
+        public static int ReadRequiredInt(string prompt)
+        {
+            while (true)
+            {
+                string input = ReadRequiredInput(prompt);
+                if (int.TryParse(input, out int value))
+                    return value;
+
+                Console.WriteLine("Error: Please enter a valid number.");
+            }
+        }
+    }
+}
